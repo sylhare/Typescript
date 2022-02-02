a6909aae6d4616162ff0dd42fb38758d8fb0bc2d
sylhare <sylhare@outlook.com>
2022-02-02 16:33:52 -0500

    Add product example

---
+++ src/models/Product.ts
@@ -0,0 +1,17 @@
+export class Product {
+    id: number = 0;
+    name: string = 'Product';
+
+    constructor(product?: Partial<Product>) {
+        Object.assign(this, product);
+    }
+
+    toString() {
+        return 'This is ' + this.name;
+    }
+}
+
+export function randomProduct() {
+    const id = Math.floor(Math.random() * 100);
+    return new Product({ id, name: `Product-${id}` });
+}
