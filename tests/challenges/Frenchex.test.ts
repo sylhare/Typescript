93893faf2f704a3488edaf03ee3f75d7c0f2f7ef
sylhare <sylhare@outlook.com>
2022-02-22 10:31:10 -0500

    With regex

---
+++ tests/challenges/Frenchex.test.ts
@@ -0,0 +1,46 @@
+describe('Pardon my french regex', () => {
+
+  function frenchex(input: string) {
+    return input.replace(/\b(\w)(\w+)\b/g, (word: string, first: string, rest: string) => {
+      if (/[aeiou]/i.test(first)) return word + 'way';
+      const groups = word.match(/\b([^\W_aeiou]+)(\w+)\b/)!!;
+
+      return groups[2].replace(/^\w/, x => /[A-Z]/.test(first) ? x.toUpperCase() : x) +
+        groups[1].toLowerCase() + 'ay';
+    });
+  }
+
+  it('should work for coffee', () => {
+    expect(frenchex('coffee')).toBe('offeecay');
+  });
+
+  it('should work for a sentence', () => {
+    expect(frenchex('have coffee')).toBe('avehay offeecay');
+  });
+
+  it('should work for a caps', () => {
+    expect(frenchex('Have Coffee')).toBe('Avehay Offeecay');
+  });
+
+  it('should not change if it starts by a vowel', () => {
+    expect(frenchex('eat oranges')).toBe('eatway orangesway');
+  });
+
+  it('should work for multiple consonant', () => {
+    expect(frenchex('striking thunder')).toBe('ikingstray underthay');
+  });
+
+  it('should work for punctuation and vowel', () => {
+    expect(frenchex('Oh!')).toBe('Ohway!');
+    expect(frenchex('oh!')).toBe('ohway!');
+  });
+
+  it('should work with punctuation', () => {
+    expect(frenchex('Have, coffee!')).toBe('Avehay, offeecay!');
+    expect(frenchex('Huh. Wat?')).toBe('Uhhay. Atway?');
+  });
+
+  it('should rearrange all consonant at the beginning', () => {
+    expect(frenchex('Who is driving this vehicle?')).toBe('Owhay isway ivingdray isthay ehiclevay?');
+  });
+});
