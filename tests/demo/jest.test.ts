ead9f5271b73e23968a7c71ec847faf5f07d752a
sylhare <sylhare@outlook.com>
2022-08-02 15:17:20 -0400

    Add .each example

--- tests/demo/jest.test.ts
+++ tests/demo/jest.test.ts
@@ -13,5 +13,22 @@ describe('It is jest an excuse', () => {
   it('should be a date', () => {
     expect(new Date()).toBeDate();
   });
+
+  it.each([null, undefined, ''])('"%s" should be falsy', (input: any) => {
+    expect(input).toBeFalsy();
+  });
+
+  describe.each([
+    { user: 'dev' },
+    { user: 'admin' },
+    { user: 'customer' }
+  ])('API', (current: { user: string }) => {
+
+    it(`${current.user} can read`, () => {
+      expect(checkRights(current)).toBeTruthy();
+    });
+
+    const checkRights = (_: { user: string }) => true;
+  });
 });
 
