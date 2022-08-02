5ab614eb00dd4e9408cb73779bd117c64b24fcbc
sylhare <sylhare@outlook.com>
2022-08-02 15:17:43 -0400

    Add examples mocks

---
+++ tests/demo/MarketService.test.ts
@@ -0,0 +1,37 @@
+import { MarketService } from '../../src/service/MarketService';
+
+describe('Market Service', () => {
+  describe('with spy on service', () => {
+    const marketService = new MarketService();
+    it('gives the info', () => {
+      expect(marketService.info()).toMatch(/buy and sell items/);
+    });
+
+    it('buy reject by default', async () => {
+      await expect(marketService.buy({ name: 'orange juice' })).rejects.toMatch(/rejects by default/);
+    });
+
+    it('spy on buy', async () => {
+      const spy = jest.spyOn(marketService, 'buy').mockImplementation(() => Promise.resolve('ok'));
+      const response = await marketService.buy({ name: 'orange juice' });
+      expect(response).toEqual('ok');
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('with mock service', () => {
+    const marketServiceMock: jest.Mocked<MarketService> = {
+      url: '',
+      buy: jest.fn().mockResolvedValue('ok'),
+      info: jest.fn().mockImplementation(() => new MarketService().info()),
+    };
+    it('gives the info', () => {
+      expect(marketServiceMock.info()).toMatch(/buy and sell items/);
+    });
+
+    it('mocks the service', async () => {
+      const response = await marketServiceMock.buy({ name: 'orange juice' });
+      expect(response).toEqual('ok');
+    });
+  });
+});
---
+++ tests/demo/Service.test.ts
@@ -0,0 +1,33 @@
+import { mMultiply, mSum } from '../../src/service/Calculator';
+import { MarketService } from '../../src/service/MarketService';
+
+jest.mock('../../src/service/Calculator', () => ({
+  ...jest.requireActual('../../src/service/Calculator'), // use actual for all non-hook parts
+  mSum: jest.fn().mockImplementation(() => 'mocked')
+}));
+
+describe('Calculator', () => {
+  it('sums', () => expect(mSum(3, 2)).toEqual('mocked'));
+  it('multiplies', () => expect(mMultiply(3, 2)).toEqual(6));
+});
+
+// ---------------------------------------------------------------------------------------
+
+jest.mock('../../src/service/MarketService', () => ({
+  MarketService: jest.fn(() => ({
+    buy: jest.fn().mockResolvedValue('ok'),
+  })),
+}));
+
+describe('Market Service', () => {
+  const marketService = new MarketService();
+
+  it('gives an error on not mocked method', () => {
+    expect(() => marketService.info()).toThrow(Error);
+  });
+
+  it('mocks the buy method', async () => {
+    const response = await marketService.buy({ name: 'orange juice' });
+    expect(response).toEqual('ok');
+  });
+});
