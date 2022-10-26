74d54facf70be2a6429e3d5dcf8741d0040fd0fd
sylhare <sylhare@outlook.com>
2022-10-26 08:56:58 -0400

    Add one level

--- tests/challenges/Word.test.ts
+++ tests/challenges/Word.test.ts
@@ -1,22 +1,26 @@
-describe('Word Cut', () => {
+describe('Word', () => {
 
-  function wordCut(input: string): Set<String> {
-    return new Set(input.replace(/[!?.,]/g, '').split(' '));
-  }
+  describe('Word Cut', () => {
+    function wordCut(input: string): Set<String> {
+      return new Set(input.replace(/[!?.,]/g, '').split(' '));
+    }
 
-  it('cuts sentence into words', () => {
-    expect([...wordCut('this is a text')]).toIncludeSameMembers(['this', 'is', 'a', 'text']);
+    it('cuts sentence into words', () => {
+      expect([...wordCut('this is a text')]).toIncludeSameMembers(['this', 'is', 'a', 'text']);
+    });
+
+    it('removes duplicates', () => {
+      expect([...wordCut('this is is a this text')]).toIncludeSameMembers(['this', 'is', 'a', 'text']);
+    });
+
+    it('removes punctuation', () => {
+      expect([...wordCut('this, is!! is a this text?')]).toIncludeSameMembers(['this', 'is', 'a', 'text']);
+    });
+
+    it('keeps apostrophes', () => {
+      expect([...wordCut('Hey! That\'s my hat hat hat!')]).toIncludeSameMembers(['Hey', 'That\'s', 'my', 'hat']);
+    });
   });
 
-  it('removes duplicates', () => {
-    expect([...wordCut('this is is a this text')]).toIncludeSameMembers(['this', 'is', 'a', 'text']);
-  });
-
-  it('removes punctuation', () => {
-    expect([...wordCut('this, is!! is a this text?')]).toIncludeSameMembers(['this', 'is', 'a', 'text']);
-  });
-
-  it('keeps apostrophes', () => {
-    expect([...wordCut('Hey! That\'s my hat hat hat!')]).toIncludeSameMembers(['Hey', 'That\'s', 'my', 'hat']);
   });
 });
