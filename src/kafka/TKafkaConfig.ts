5e260486dd68515021d2bf5784ba6da636549f40
sylhare <sylhare@outlook.com>
2022-12-07 15:43:12 -0500

    Update config

--- src/kafka/TKafkaConfig.ts
+++ src/kafka/TKafkaConfig.ts
@@ -4,8 +4,8 @@ export class TKafkaConfig implements KafkaConfig {
   readonly brokers: string[];
   readonly clientId: string;
 
-  constructor(public readonly serviceName: string, env: any) {
+  constructor(clientId: string, env: any) {
     this.brokers = env.KAFKA_BOOTSTRAP_SERVERS?.split(',') || ['localhost:9092'];
-    this.clientId = serviceName;
+    this.clientId = clientId;
   }
 }
