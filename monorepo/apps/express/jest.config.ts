b3d8bec291b1c8610200d5c7d4f22b4f3ab3ee99
sylhare <sylhare@outlook.com>
2023-06-15 09:43:18 -0400

    Add jest config

---
+++ monorepo/apps/express/jest.config.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line jest/no-jest-import
+import type { Config } from 'jest';
+
+import config from '../../jest.config';
+
+const subConfig: Config = {
+  ...config,
+};
+
+export default subConfig;
