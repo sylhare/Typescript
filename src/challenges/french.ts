92c2c3d3e9f5e6b4664627006745ecd806fa3a8e
sylhare <sylhare@outlook.com>
2024-08-30 14:00:58 -0400

    Update french example

---
+++ src/challenges/french.ts
@@ -0,0 +1,24 @@
+export function french(input: string): string {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  const special = [',', '.', '?', '!'];
+
+  return input.split(' ').map(word => {
+    const suffix = word.slice(0, [...word].findIndex(e => vowels.includes(e)));
+    let  prefix =  word.slice(suffix.length, suffix.length + 1), middle = '', end = '';
+    [...word].forEach(char => special.includes(char) ? end += char : middle += char);
+
+    if (vowels.includes(word[0].toLowerCase())) return middle + 'way' + end;
+    if (suffix[0].toUpperCase() === suffix[0]) prefix = prefix.toUpperCase();
+    return prefix + middle.slice(suffix.length + 1) + suffix.toLowerCase() + 'ay' + end;
+  }).join(' ');
+}
+
+export function frenchex(input: string) {
+  return input.replace(/\b(\w)(\w+)\b/g, (word: string, first: string, _: string) => {
+    const groups = word.match(/\b([^\W_aeiou]+)(\w+)\b/)!!;
+
+    return /[aeiou]/i.test(first) ? word + 'way' :
+      groups[2].replace(/^\w/, x => /[A-Z]/.test(first) ? x.toUpperCase() : x) +
+      groups[1].toLowerCase() + 'ay';
+  });
+}
--- tests/challenges/French.test.ts
+++ tests/challenges/French.test.ts
@@ -1,51 +1,43 @@
+import { french, frenchex } from '../../src/challenges/french';
+
 describe('Pardon my french', () => {
+  describe.each([
+    { solution: french },
+    { solution: frenchex },
+  ])('Using %s', ({ solution }) => {
 
-  function french(input: string): string {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-    const special = [',', '.', '?', '!'];
+    it('should work for coffee', () => {
+      expect(solution('coffee')).toBe('offeecay');
+    });
 
-    return input.split(' ').map(word => {
-      const suffix = word.slice(0, [...word].findIndex(e => vowels.includes(e)));
-      let  prefix =  word.slice(suffix.length, suffix.length + 1), middle = '', end = '';
-      [...word].forEach(char => special.includes(char) ? end += char : middle += char);
+    it('should work for a sentence', () => {
+      expect(solution('have coffee')).toBe('avehay offeecay');
+    });
 
-      if (vowels.includes(word[0].toLowerCase())) return middle + 'way' + end;
-      if (suffix[0].toUpperCase() === suffix[0]) prefix = prefix.toUpperCase();
-      return prefix + middle.slice(suffix.length + 1) + suffix.toLowerCase() + 'ay' + end;
-    }).join(' ');
-  }
+    it('should work for a caps', () => {
+      expect(solution('Have Coffee')).toBe('Avehay Offeecay');
+    });
 
-  it('should work for coffee', () => {
-    expect(french('coffee')).toBe('offeecay');
-  });
+    it('should not change if it starts by a vowel', () => {
+      expect(solution('eat oranges')).toBe('eatway orangesway');
+    });
 
-  it('should work for a sentence', () => {
-    expect(french('have coffee')).toBe('avehay offeecay');
-  });
+    it('should work for multiple consonant', () => {
+      expect(solution('striking thunder')).toBe('ikingstray underthay');
+    });
 
-  it('should work for a caps', () => {
-    expect(french('Have Coffee')).toBe('Avehay Offeecay');
-  });
+    it('should work for punctuation and vowel', () => {
+      expect(solution('Oh!')).toBe('Ohway!');
+      expect(solution('oh!')).toBe('ohway!');
+    });
 
-  it('should not change if it starts by a vowel', () => {
-    expect(french('eat oranges')).toBe('eatway orangesway');
-  });
+    it('should work with punctuation', () => {
+      expect(solution('Have, coffee!')).toBe('Avehay, offeecay!');
+      expect(solution('Huh. Wat?')).toBe('Uhhay. Atway?');
+    });
 
-  it('should work for multiple consonant', () => {
-    expect(french('striking thunder')).toBe('ikingstray underthay');
-  });
-
-  it('should work for punctuation and vowel', () => {
-    expect(french('Oh!')).toBe('Ohway!');
-    expect(french('oh!')).toBe('ohway!');
-  });
-
-  it('should work with punctuation', () => {
-    expect(french('Have, coffee!')).toBe('Avehay, offeecay!');
-    expect(french('Huh. Wat?')).toBe('Uhhay. Atway?');
-  });
-
-  it('should rearrange all consonant at the beginning', () => {
-    expect(french('Who is driving this vehicle?')).toBe('Owhay isway ivingdray isthay ehiclevay?');
+    it('should rearrange all consonant at the beginning', () => {
+      expect(solution('Who is driving this vehicle?')).toBe('Owhay isway ivingdray isthay ehiclevay?');
+    });
   });
 });
