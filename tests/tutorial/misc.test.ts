53656f26d08efd593fec9dbf08ab47414f0ac9f9
sylhare <sylhare@outlook.com>
2024-10-06 10:32:35 -0400

    Add map test

--- tests/tutorial/misc.test.ts
+++ tests/tutorial/misc.test.ts
@@ -19,4 +19,34 @@ describe('Tests', () => {
       expect(result).toEqual(true);
     });
   });
+
+  describe('Map', () => {
+    let map: Map<string, number>;
+
+    beforeEach(() => map = new Map());
+
+    it('targetMap size with no duplicates', () => {
+      const target = 'abc';
+      map = new Map([...target].map(char => [char, (map.get(char) || 0) + 1]));
+      expect(map.size).toBe(3);
+    });
+
+    it('targetMap size with duplicates', () => {
+      const target = 'aabc';
+      map = new Map([...target].map(char => [char, (map.get(char) || 0) + 1]));
+      expect(map.size).toBe(3);
+    });
+
+    it('targetMap size with all duplicates', () => {
+      const target = 'aaaa';
+      map = new Map([...target].map(char => [char, (map.get(char) || 0) + 1]));
+      expect(map.size).toBe(1);
+    });
+
+    it('targetMap size with no characters', () => {
+      const target = '';
+      map = new Map([...target].map(char => [char, (map.get(char) || 0) + 1]));
+      expect(map.size).toBe(0);
+    });
+  });
 });
