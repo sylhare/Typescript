90d2748cd8034ff36969c79b03ebec4f27a716d3
sylhare <sylhare@outlook.com>
2025-03-29 13:15:30 -0400

    Add closest

--- src/challenges/partition/closest.ts
+++ src/challenges/partition/closest.ts
@@ -10,3 +10,12 @@ export function closestIndex (arr: number[], target: number): number {
   }
   return left;
 }
+
+export function closest(arr: number[], target: number): number {
+  const index = closestIndex(arr, target);
+  if (index === 0) return arr[0];
+  if (index === arr.length) return arr[arr.length - 1];
+  const below = arr[index - 1];
+  const above = arr[index];
+  return Math.abs(Math.floor(below - target)) < Math.abs(Math.floor(above - target)) ? below : above;
+}
--- tests/challenges/partition/closest.test.ts
+++ tests/challenges/partition/closest.test.ts
@@ -1,6 +1,6 @@
-import { closestIndex } from '../../../src/challenges/partition/closest';
+import { closest, closestIndex } from '../../../src/challenges/partition/closest';
 
-describe('Closest index', () => {
+describe('Closest', () => {
 
   describe.each([
     { method: closestIndex, name: 'bisectLeft' },
@@ -24,6 +24,31 @@ describe('Closest index', () => {
       const target = 3.5;
       const result = method(arr, target);
       expect(result).toBe(3); // arr[3] === 4 closest to 3.5
+    });
+  });
+
+  describe.each([
+    { method: closest, name: 'closest' },
+  ])('Method: $name', ({ method }) => {
+    it('should find the closest number to the target in an array with negative numbers', () => {
+      const arr = [-10, -5, -2, -1];
+      const target = -3;
+      const result = method(arr, target);
+      expect(result).toBe(-2); // -2 closest to -3
+    });
+
+    it('should find the closest number to the target in an array with mixed numbers', () => {
+      const arr = [-10, -5, 0, 5, 10];
+      const target = 3;
+      const result = method(arr, target);
+      expect(result).toBe(5); // 5 closest to 3
+    });
+
+    it('should find the closest number to the target in an array with positive numbers', () => {
+      const arr = [1, 2, 3, 4, 5];
+      const target = 3.5;
+      const result = method(arr, target);
+      expect(result).toBe(4); // 4 closest to 3.5
     });
   });
 });
