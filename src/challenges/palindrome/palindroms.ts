3700e129736698981abbedae15fa83e29380463c
sylhare <sylhare@outlook.com>
2025-05-02 16:45:55 -0400

    Update naming

--- src/challenges/palindrome/palindroms.ts
+++ src/challenges/palindrome/palindroms.ts
@@ -60,11 +60,7 @@ function search(word: string, wordIndex: number, root: TrieNode, result: number[
 export function isPalindrome(word: string, start: number): boolean {
   let left = start, right = word.length - 1;
   while (left < right) {
-    if (word[left] !== word[right]) {
-      return false;
-    }
-    left++;
-    right--;
+    if (word[left++] !== word[right--]) return false;
   }
   return true;
 }
