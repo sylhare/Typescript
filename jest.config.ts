5fdbabfba8969adef678d5e43b511b9a6df51020
sylhare <sylhare@outlook.com>
2025-11-12 10:17:58 -0500

    Update jest config to typescript

---
+++ jest.config.ts
@@ -0,0 +1,61 @@
+import type { Config } from 'jest';
+
+const projects = [
+  {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    displayName: 'challenges',
+    testMatch: ['<rootDir>/tests/challenges/**/*.test.ts'],
+    collectCoverageFrom: ['src/challenges/**/*.ts'],
+  },
+  {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    displayName: 'models',
+    testMatch: ['<rootDir>/tests/object/**/*.test.ts'],
+    collectCoverageFrom: ['src/models/**/*.ts'],
+  },
+  {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    displayName: 'service',
+    testMatch: ['<rootDir>/tests/jest-demo/**/*.test.ts'],
+    collectCoverageFrom: ['src/service/**/*.ts'],
+  },
+  {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    displayName: 'tutorial',
+    testMatch: ['<rootDir>/tests/tutorial/**/*.test.ts'],
+    collectCoverageFrom: ['src/tutorial/**/*.ts'],
+  },
+];
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '\\.(test|e2e)\\.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  setupFilesAfterEnv: [
+    'jest-extended/all'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
+  coveragePathIgnorePatterns: [],
+  coverageReporters: ['text', 'text-summary', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      lines: 85
+    }
+  },
+  watchPathIgnorePatterns: ['.idea'],
+  modulePathIgnorePatterns: ['<rootDir>/project/', '<rootDir>/monorepo-example/'],
+  projects,
+};
+
+export default config;
+
