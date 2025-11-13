c598fa84a2d24d336cdc9f51f824f5078b0806fe
sylhare <sylhare@outlook.com>
2025-11-13 13:30:39 -0500

    Update test config

--- monorepo-example/jest.config.ts
+++ monorepo-example/jest.config.ts
@@ -13,12 +13,40 @@ const config: Config = {
     '/node_modules/',
     '/dist/',
   ],
-  moduleNameMapper: {
-    '^@sylhare/kafka$': '<rootDir>/packages/kafka/src',
-    '^@sylhare/redpanda$': '<rootDir>/packages/redpanda/src',
-    '^@sylhare/is-even$': '<rootDir>/packages/is-even',
-    '^@sylhare/is-odd$': '<rootDir>/packages/is-odd'
-  }
+  projects: [
+    {
+      displayName: 'general',
+      testMatch: [
+        '<rootDir>/packages/is-even/**/*.test.ts',
+        '<rootDir>/packages/is-odd/**/*.test.ts',
+        '<rootDir>/packages/kafka/**/*.test.ts',
+        '<rootDir>/packages/redpanda/**/*.test.ts'
+      ],
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      moduleNameMapper: {
+        '^@acme-corp/(.*)$': '<rootDir>/packages/$1/',
+        '^@sylhare/kafka$': '<rootDir>/packages/kafka/src',
+        '^@sylhare/redpanda$': '<rootDir>/packages/redpanda/src',
+        '^@sylhare/is-even$': '<rootDir>/packages/is-even',
+        '^@sylhare/is-odd$': '<rootDir>/packages/is-odd'
+      }
+    },
+    {
+      displayName: 'express',
+      testMatch: ['<rootDir>/packages/express/**/*.test.ts'],
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      transform: {
+        '^.+\\.ts$': ['ts-jest', {
+          tsconfig: '<rootDir>/packages/express/tsconfig.pkg.json'
+        }]
+      },
+      moduleNameMapper: {
+        '^@sylhare/example$': '<rootDir>/packages/example/src'
+      }
+    }
+  ]
 };
 
 export default config;
